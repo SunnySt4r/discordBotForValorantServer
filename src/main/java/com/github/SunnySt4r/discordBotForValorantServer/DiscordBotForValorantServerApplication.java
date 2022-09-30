889b12d114eab9c48a3eae9814effa67406876aa
SunnySt4r <door011@mail.ru>
2022-09-30 11:43:59 +0300

    first commit

---
+++ src/main/java/com/github/SunnySt4r/discordBotForValorantServer/DiscordBotForValorantServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.SunnySt4r.discordBotForValorantServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordBotForValorantServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordBotForValorantServerApplication.class, args);
+	}
+
+}
